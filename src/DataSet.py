ebb346736abab7b7020af1e71ff5078f085b2d7b
Fella Belkham <fichette@users.noreply.github.com>
2017-01-16 22:35:13 +0100

    Add files via upload

---
+++ src/DataSet.py
@@ -0,0 +1,37 @@
+import os
+import math
+import random
+
+"""
+   Function: LabelData 
+   Input: Name of the database, either ORL or Shuffield, note that both 
+   Output: Train set and test set
+   
+   Description: The function retreive the images from each ind
+   
+"""
+def LabelData(dataset):
+    dataDir = "../Dataset/"+dataset+"/"
+    listDir = os.listdir(dataDir)
+    train_set = []
+    test_set = []
+    classId = 0
+    
+    for individuDir in listDir: 
+        
+        list_img = os.listdir(dataDir+ individuDir)
+        t_size = math.floor(len(list_img)*0.7)
+        random.shuffle(list_img)
+
+        for img in list_img:
+                
+            tup = (dataDir+ individuDir+"/" + img, classId)
+            if t_size >0:
+                train_set.append(tup)
+                t_size -= 1
+            else:
+                test_set.append(tup)
+
+        classId += 1 
+    return train_set, test_set 
+LabelData("orl")
